662a7d0c1730a1eca27248d61249de66a53b3d19
Alexandra Savchenko <svchhh@yandex.ru>
2022-09-17 22:03:29 +0300

    add eventListener on scroll

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,28 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const NavBar = () => {
     const [activeLink, setActiveLink] = useState("home");
+    const [scrolled, setScrolled] = useState(false);
     
+    useEffect(()=> {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
+
     return (
-        <Navbar expand="lg">
+        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
         <Container>
             <Navbar.Brand href="#home">
             <img src={""} alt="Logo" />
@@ -17,9 +32,18 @@ const NavBar = () => {
             </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#skills">Skills</Nav.Link>
-                <Nav.Link href="#projects">Projects</Nav.Link>
+                <Nav.Link 
+                    href="#home" 
+                    className={activeLink === "home" ? "active navbar-link" : "navbar-link"}
+                >Home</Nav.Link>
+                <Nav.Link 
+                    href="#skills" 
+                    className={activeLink === "skills" ? "active navbar-link" : "navbar-link"}
+                >Skills</Nav.Link>
+                <Nav.Link 
+                    href="#projects" 
+                    className={activeLink === "projects" ? "active navbar-link" : "navbar-link"}
+                >Projects</Nav.Link>
             </Nav>
             <span className="navbar-text">
                 <div className="social-icon">
