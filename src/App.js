3c06dd73fd575003b9322dbdc195f1204e9c2352
Alexandra Savchenko <svchhh@yandex.ru>
2022-09-21 13:32:41 +0300

    delete Contact block

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
-      <Contact />
     </div>
   );
 }
